f5f8a7943feef3820cbcb0c8bb3dee836e62dbc3
Carol St.Louis <stlouis_c@yahoo.com>
2016-03-21 23:31:01 -0700

    added template for variables exercise

---
+++ variables_ex_01/main.go
@@ -0,0 +1,23 @@
+// Declare three variables that are initalized to their zero value and three
+// declared with a literal value. Declare variables of type string, int and
+// bool. Display the values of those variables.
+//
+// Declare a new variable of type float32 and initalize the variable by
+// converting the literal value of Pi (3.14).
+package main
+
+// main is the entry point for the application.
+func main() {
+	// Declare variables that are set to their zero value.
+
+	// Display the value of those variables.
+
+	// Declare variables and initalize.
+	// Using the short variable declaration operator.
+
+	// Display the value of those variables.
+
+	// Perform a type conversion.
+
+	// Display the value of that variable.
+}
